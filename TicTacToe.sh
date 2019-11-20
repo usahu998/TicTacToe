d28d40799d6d46ebac1c3586b0c39e1e3bd5881e
Upendra Sahu <usahu998@gmail.com>
2019-11-21 01:21:51 +0530

    Add : initialized Empty Board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Welcome to Tic-Tac-Toe game"
+
+
+#FINAL_VARIABLES
+MAX_BOARD_POSITION=9
+
+#BOARD ARRAY
+declare -a board_Position
+
+
+function initialising_Board()
+{
+	for (( i=1; i<=$MAX_BOARD_POSITION; i++ ))
+	do
+		board_Position[$i]=0
+	done
+}
