35ce591a083cae7bb4c4680e0813ae46682fbf11
Upendra Sahu <usahu998@gmail.com>
2019-11-21 01:44:21 +0530

    Add : functionality to check if Game is Tie or not

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -5,28 +5,18 @@ echo "Welcome to Tic-Tac-Toe game"
 
 #FINAL_VARIABLES
 MAX_BOARD_POSITION=9
-PLAYER_SYMBOL=''
-COMP_SYMBOL=''
 
 
 #VARIABLES
-whoPlays=0
-
-
-#BOARD ARRAY
-#!/bin/bash
-
-echo "Welcome to Tic-Tac-Toe game"
-
-
-#FINAL_VARIABLES
-MAX_BOARD_POSITION=9
-
-#VARIABLES
-whoPlays=0
 playerPosition=0
 playerSymbol=''
 computerSymbol=''
+
+
+#boolean Flags
+someoneWon=false
+whoPlays=false
+
 
 #BOARD ARRAY
 declare -a boardPosition
@@ -36,7 +26,7 @@ function initializingBoard()
 {
 	for (( i=1; i<=$MAX_BOARD_POSITION; i++ ))
 	do
-		boardPosition[$i]=0
+		boardPosition[$i]='-'
 	done
 }
 
@@ -48,7 +38,7 @@ function symbolAssignment()
 
 	if [ $firstPlay -eq 1 ]
 	then
-		(( whoPlays++ ))
+		whoPlays=true
 		playerSymbol='X'
 		computerSymbol='O'
 		echo "Player symobol : X | Computer symbol : O"
@@ -58,19 +48,6 @@ function symbolAssignment()
 		computerSymbol='X'
 		echo "Player symobol : O | Computer symbol : X"
 		echo "Computer Plays First"
-	fi
-}
-
-
-function playerInput()
-{
-	read -p "Enter  position Number to put $playerSymbol at Empty Position " playerPosition
-	if [ ${boardPosition[$playerPosition]} -eq 0 ]
-	then
-		boardPosition[$playerPosition]=$playerSymbol
-	else
-		echo "Position Occupied Please enter new Position"
-		playerInput
 	fi
 }
 
@@ -89,7 +66,121 @@ function displayBoard()
 	echo "     |     |   "
 }
 
+
+
+function playerInput()
+{
+	read -p "Enter  position Number to put $playerSymbol at Empty Position " playerPosition
+	if [ ${boardPosition[$playerPosition]} == '-' ]
+	then
+		boardPosition[$playerPosition]=$playerSymbol
+	else
+		echo "Position Occupied Please enter new Position"
+		playerInput
+	fi
+}
+
+
+function checkHorizontalWinner()
+{
+	max_number_rows=3
+	position=1
+	loopCounter=1
+	while [  $loopCounter -le 3 ]
+	do
+		if [[ ${boardPosition[$position]} == ${boardPosition[$position+1]} ]] && [[  ${boardPosition[$position+1]}  ==  ${boardPosition[$position+2]} ]] && [[ ${boardPosition[$position+2]} == $1 ]]
+		then
+			displayBoard
+			echo " HURRAY!!! $1 wins "
+			someoneWon=true
+			break
+		else
+			position=$(( $position+$max_number_rows ))
+		fi
+		loopCounter=$(($loopCounter+1))
+	done
+}
+
+
+
+function checkVerticalWinner()
+{
+	position=1
+	loopCounter=1
+	while [ $loopCounter -le 3 ]
+	do
+		if [[ ${boardPosition[$position]} == ${boardPosition[$position+3]} ]] && [[  ${boardPosition[$position+3]}  ==  ${boardPosition[$position+6]} ]] && [[ ${boardPosition[$position+6]} == $1 ]]
+		then
+			displayBoard
+			echo " HURRAY!!! $1 wins "
+			someoneWon=true
+			break
+		else
+			position=$(($position+1))
+		fi
+		loopCounter=$(($loopCounter+1))
+	done
+}
+
+
+
+function checkDiagonalWinner()
+{
+	position=1
+	loopCounter=1
+	while [ $loopCounter -le 2 ]
+	do
+		if [[ ${boardPosition[$position]} == ${boardPosition[$position+4]} ]] && [[  ${boardPosition[$position+4]}  ==  ${boardPosition[$position+8]} ]] && [[ ${boardPosition[$position+8]} == $1 ]]
+		then
+			displayBoard
+			echo " HURRAY!!! $1 wins "
+			someoneWon=true
+			break
+		elif [[ ${boardPosition[$position+2]} == ${boardPosition[$position+4]} ]] && [[  ${boardPosition[$position+4]}  ==  ${boardPosition[$position+6]} ]] && [[ ${boardPosition[$position+6]} == $1 ]]
+		then
+			displayBoard
+			echo " HURRAY!!! $1 wins "
+			someoneWon=true
+			break
+		fi
+		loopCounter=$(($loopCounter+1))
+	done
+}
+
+
+function checkGameTie()
+{
+	count=1
+	while [ ${boardPosition[$count]} != '-' ]
+	do
+		if [ $count -eq 9 ]
+		then
+			displayBoard
+			echo "Game Is tie"
+			someoneWon=true
+			break
+		else
+			count=$($count+1)
+		fi
+	done
+}
+
+
+
+
+
+#-------------------------MAIN-----------------
+
+
 initializingBoard
-displayBoard
 symbolAssignment
-playerInput
+
+while [ $someoneWon == false ]
+do
+	displayBoard
+	playerInput
+	checkHorizontalWinner $playerSymbol
+	checkVerticalWinner $playerSymbol
+	checkDiagonalWinner $playerSymbol
+	checkGameTie
+done
