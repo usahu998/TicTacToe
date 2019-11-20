ef5bca3fdc246737bfe78bf2765e1052b4e20437
Upendra Sahu <usahu998@gmail.com>
2019-11-21 01:27:30 +0530

    Add : Assigned symbol to Player And Computer

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -5,6 +5,13 @@ echo "Welcome to Tic-Tac-Toe game"
 
 #FINAL_VARIABLES
 MAX_BOARD_POSITION=9
+PLAYER_SYMBOL=''
+COMP_SYMBOL=''
+
+
+#VARIABLES
+whoPlays=0
+
 
 #BOARD ARRAY
 declare -a board_Position
@@ -16,4 +23,22 @@ function initialising_Board()
 	do
 		board_Position[$i]=0
 	done
+}
+
+
+
+function symbol_Assignment()
+{
+	firstPlay=$((RANDOM%2))
+
+	if [ $firstPlay -eq 1 ]
+	then
+		PLAYER_SYMBOL='X'
+		COMP_SYMBOL='O'
+		echo "Player symobol : X | Computer symbol : O"
+	else
+		PLAYER_SYMBOL='O'
+		COMP_SYMBOL='X'
+		echo "Player symobol : O | Computer symbol : X"
+	fi
 }
